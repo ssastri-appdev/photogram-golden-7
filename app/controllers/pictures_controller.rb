361df3653a96fd180e089142e0e10c13cb2ca669
Siddharth Sastri <ssastri@chicagobooth.edu>
2018-10-30 20:52:14 +0000

    golden 7 photogram

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,11 +8,18 @@ class PicturesController < ApplicationController
     end
     
     def new_form
-    render("pic_templates/enter_new_photo.html.erb")
+        render("pic_templates/enter_new_photo.html.erb")
     end
     
     def create_row
-    render("pic_templates/show_new_photo.html.erb")
+    p=Photo.new
+    p.source=params.fetch("the_source")
+    p.caption=params.fetch("the_caption")
+    p.save
+    @photo_list=Photo.all
+    n=@photo_list.length
+    redirect_to("https://photogram-ssastri.c9users.io/photos")
+    
     end
      
     def show_details
@@ -24,15 +31,33 @@ class PicturesController < ApplicationController
     end
     
     def edit_row
+    the_id=params.fetch("id_num_two")
+    the_pho = Photo.find(the_id)
+    @photo_source=the_pho.source
+    @photo_caption=the_pho.caption
+    @photo_id=the_pho.id
     render("pic_templates/edit_a_photo.html.erb")
     end
     
     def show_update
-    render("pic_templates/show_photo_update.html.erb")
+    the_id=params.fetch("id_num_three")
+    pho_ud = Photo.find(the_id)
+    pho_ud.source=params.fetch("the_source")
+    pho_ud.caption=params.fetch("the_caption")
+    pho_ud.save
+    @photo_source=pho_ud.source
+    @photo_caption=pho_ud.caption
+    redirect_to("https://photogram-ssastri.c9users.io/photos/#{the_id}")
+
+    # render("pic_templates/show_photo_update.html.erb")
     end
     
     def delete_row
-    render("pic_templates/photo_delete.html.erb")
+    the_id=params.fetch("id_num_four")
+    the_photo = Photo.find(the_id)
+    the_photo.destroy
+    redirect_to("https://photogram-ssastri.c9users.io/photos")
+    #render("pic_templates/photo_delete.html.erb")
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ get("/photos/new", {:controller => "pictures", :action =>"new_form"})
 get("/photos/create_photo", {:controller => "pictures", :action =>"create_row"})
 get("/photos/:id_num_one", {:controller => "pictures", :action =>"show_details"})
 get("/photos/:id_num_two/edit", {:controller => "pictures", :action =>"edit_row"})
-get("/photos/update_photo/:id_num_three", {:controller => "pictures", :action =>"show_update"})
-get("/photos/delete_photo/:id_num_four", {:controller => "pictures", :action =>"delete_row"})
+get("/update_photo/:id_num_three", {:controller => "pictures", :action =>"show_update"})
+get("delete_photo/:id_num_four", {:controller => "pictures", :action =>"delete_row"})
 
  
  
